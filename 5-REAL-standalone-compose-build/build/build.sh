89782e5e171e1f861f07c10093ee3eb4b0ae0e6e
kior <jooozip@yonsei.ac.kr>
2025-08-25 02:13:19 +0900

    upd. release 20250825-0213

--- 5-REAL-standalone-compose-build/build/build.sh
+++ 5-REAL-standalone-compose-build/build/build.sh
@@ -1,2 +1,3 @@
 # internal-registry Build Command
-DOCKER_BUILDKIT=0 docker build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-webtob:v5 -f ./DockerFile.webtob5 ../
+docker build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-webtob:v5 -f ./DockerFile.webtob5 ../
+#podman build --platform linux/amd64 --no-cache --force-rm -t tmaxsoft-webtob:v5 -f ./DockerFile.webtob5 ../
