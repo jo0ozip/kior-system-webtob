42f3df362bf9d4cd2ff11bfe9ad40c9e538fbc4c
kior <jooozip@yonsei.ac.kr>
2025-08-25 01:59:07 +0900

    upd. release 20250825-0200

--- 5-REAL-standalone-compose-build/env/bin/wboot.sh
+++ 5-REAL-standalone-compose-build/env/bin/wboot.sh
@@ -7,13 +7,13 @@ if [ -z ${HOSTNAME} ]; then export HOSTNAME=`hostname`; fi
 sed -i "s/%HOSTNAME%/${HOSTNAME}/g" ${WEBTOBDIR}/config/http.m
 
 if [ -z ${WEBTOBDIR} ]; then export WEBTOBDIR="CHECK WEBTOBDIR"; fi
-sed -i "s/%WEBTOBDIR%/${WEBTOBDIR}/g" ${WEBTOBDIR}/config/http.m
+sed -i "s|%WEBTOBDIR%|${WEBTOBDIR}|g" ${WEBTOBDIR}/config/http.m
 
 if [ -z ${DOCROOT} ]; then export DOCROOT="${WEBTOBDIR}/docs"; fi
-sed -i "s/%DOCROOT%/${DOCROOT}/g" ${WEBTOBDIR}/config/http.m
+sed -i "s|%DOCROOT%|${DOCROOT}|g" ${WEBTOBDIR}/config/http.m
 
 if [ -z ${WEBTOB_LOG_HOME} ]; then export DOCROOT="${WEBTOBDIR}/log"; fi
-sed -i "s/%WEBTOB_LOG_HOME%/${WEBTOB_LOG_HOME}/g" ${WEBTOBDIR}/config/http.m
+sed -i "s|%WEBTOB_LOG_HOME%|${WEBTOB_LOG_HOME}|g" ${WEBTOBDIR}/config/http.m
 
 
 
